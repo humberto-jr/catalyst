ab58d781ee36144b213e40eab3b0c6d5a6fe7ae1
humberto-jr <dogmacentral@hotmail.com>
2024-04-22 04:34:23 -0700

    Adding the mpi_print.cc test that checks the mpi::print_line() member.

---
+++ testbeds/mpi_print.cc
@@ -0,0 +1,18 @@
+#include "modules/essentials.h"
+#include "modules/libmpi.h"
+
+int main(int argc, char *argv[])
+{
+	mpi::frontend mpi(&argc, &argv);
+
+	if (mpi.rank() == mpi::MASTER_PROCESS_RANK) {
+		print::line("# Ordered MPI printing");
+		print::line("# Build details:");
+		mpi::about();
+	}
+
+	// NOTE: This must be printed in order.
+	mpi.print_line("Rank ", mpi.rank(), " says: \"Hello, World!\"");
+
+	return EXIT_SUCCESS;
+}
