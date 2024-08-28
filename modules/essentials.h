945220f4b5cf32a8f3cf65bda6ba679da591df6b
humberto-jr <dogmacentral@hotmail.com>
2024-08-28 07:45:28 -0700

    Adding the thread_count() global function and the MAX_HOST_THREAD_COUNT macro.

--- modules/essentials.h
+++ modules/essentials.h
@@ -4,6 +4,24 @@
 	#include "libc.h"
 	#include "types.h"
 
+	#if defined(_OPENMP) && !defined(MAX_HOST_THREAD_COUNT)
+		#define MAX_HOST_THREAD_COUNT 256u
+	#else
+		#define MAX_HOST_THREAD_COUNT 1u
+	#endif
+
+	[[maybe_unused]]
+	static inline u32 thread_count()
+	{
+		#if defined(_OPENMP)
+			// NOTE: This must be less than or equal to
+			// the value set in MAX_HOST_THREAD_COUNT.
+			return as_u32(omp_get_num_threads());
+		#else
+			return 1u;
+		#endif
+	}
+
 	[[maybe_unused]]
 	static inline u32 thread_id()
 	{
