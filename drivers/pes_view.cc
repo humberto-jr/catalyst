462906847782c063b7b634beabe9285c487a41fe
humberto-jr <dogmacentral@hotmail.com>
2024-10-20 06:36:31 -0700

    Moving the driver's input boilerplate code into reusable helper functions (3).

--- drivers/pes_view.cc
+++ drivers/pes_view.cc
@@ -1,99 +1,44 @@
 #include "modules/essentials.h"
-#include "modules/input.h"
-#include "modules/pes.h"
-#include "key.h"
+#include "user.h"
 
-constexpr u8 PAD = 4;
+#define SHIFT_AND_SCALE(val) (((val) + shift)*scale)
+
+constexpr u8 PAD = 24;
 
 int main()
 {
 	//
-	// Vibrational grid, r:
+	// (r, R, theta)-grid:
 	//
 
-	u32 n_min = input::keyword(key::rovib_grid_start, 0u, u32_max, 0u);
+	const Range<f64> r_list = user::vibration_grid();
 
-	u32 r_count = input::keyword(key::rovib_grid_size, 1u, u32_max, 100u);
+	const Range<f64> R_list = user::scattering_grid();
 
-	f64 r_min = input::keyword(key::r_min, 0.0, f64_max, 0.5);
-
-	f64 r_max = input::keyword(key::r_max, r_min, f64_max, r_min + 100.0);
-
-	f64 r_step = (r_max - r_min)/as_f64(r_count);
+	const Range<f64> theta_list = user::polar_angle_grid();
 
 	//
-	// Scattering grid, R:
+	// Diatomic rotation quantum numbers:
 	//
 
-	u32 m_min = input::keyword(key::scatt_grid_start, 0u, u32_max, 0u);
-
-	u32 R_count = input::keyword(key::scatt_grid_size, 1u, u32_max, 100u);
-
-	f64 R_min = input::keyword(key::R_min, 0.0, f64_max, 0.5);
-
-	f64 R_max = input::keyword(key::R_max, R_min, f64_max, R_min + 100.0);
-
-	f64 R_step = (R_max - R_min)/as_f64(R_count);
+	const Range<u32> j_list = user::rotation_quantum_numbers();
 
 	//
-	// Angular grid, theta:
+	// Arrangement (a = 1, b = 2, c = 3) and PES:
 	//
 
-	u32 p_min = input::keyword(key::theta_grid_start, 0u, u32_max, 0u);
+	const char arrang = user::arrangement();
 
-	u32 theta_count = input::keyword(key::theta_grid_size, 1u, u32_max, 181u);
+	pes::Frontend pes = user::extern_pes();
 
-	f64 theta_min = input::keyword(key::theta_min, 0.0, 180.0, 0.0);
-
-	f64 theta_max = input::keyword(key::theta_max, theta_min, 180.0, 180.0);
-
-	f64 theta_step = (theta_max - theta_min)/as_f64(theta_count);
-
-	//
-	// Rotational quantum number, j:
-	//
-
-	u32 j_min = input::keyword(key::j_min, 0u, u32_max, 0u);
-
-	u32 j_max = input::keyword(key::j_max, j_min, u32_max, j_min);
-
-	u32 j_step = input::keyword(key::j_step, 1u, u32_max, 1u);
-
-	//
-	// Legendre multipole expansion, lambda:
-	//
-
-	u32 lambda_min = input::keyword(key::lambda_min, 0u, u32_max, 0u);
-
-	u32 lambda_max = input::keyword(key::lambda_max, lambda_min, u32_max, lambda_min + 20u);
-
-	u32 lambda_step = input::keyword(key::lambda_step, 0u, u32_max, 0u);
-
-	//
-	// Atomic masses and arrangement (a = 1, b = 2, c = 3):
-	//
-
-	const char arrang = as_char(96 + input::keyword(key::arrangement, 1, 3, 1));
-
-	const auto atom_a = input::keyword(key::atom_a, nist::Isotope::atom_unknown);
-	const auto atom_b = input::keyword(key::atom_b, nist::Isotope::atom_unknown);
-	const auto atom_c = input::keyword(key::atom_c, nist::Isotope::atom_unknown);
-
-	//
-	// PES:
-	//
-
-	const String pesname = input::keyword(key::extern_pes_filename);
-
-	pes::Frontend pes(pesname, atom_a, atom_b, atom_c);
+	const Range<u32> lambda_list = user::multipole_terms(0u);
 
 	//
 	// Energy scale and shift:
 	//
 
-	f64 shift = input::keyword(key::energy_shift, -f64_max, f64_max, 0.0);
-
-	f64 scale = input::keyword(key::energy_scale, -f64_max, f64_max, 1.0);
+	f64 shift = user::energy_shift();
+	f64 scale = user::energy_scale();
 
 	//
 	// Summary:
@@ -103,54 +48,53 @@ int main()
 	print::line("# Atom a: ", pes.atom_a());
 	print::line("# Atom b: ", pes.atom_b());
 	print::line("# Atom c: ", pes.atom_c());
-	print::line("# ArRangement: ", arrang);
-	print::line("# Shift: ", shift);
-	print::line("# Scale: ", scale);
+	print::line("# Arrangement: ", arrang);
+	print::line("# Energy shift: ", shift);
+	print::line("# Energy scale: ", scale);
 	print::line("# PES shared library: ", pes.filename());
 
-	if ((lambda_step > 0) && (shift != 0.0)) {
-		print::line("# WARNING: Printing Legendre multipoles with a shift applied.");
+	if ((lambda_list.count() > 0) && (shift != 0.0)) {
+		print::line("# WARNING: Printing Legendre multipoles with a shift applied");
 	}
 
 	//
-	// Theta-only print:
+	// Theta-only printing:
 	//
 
-	if ((lambda_step == 0) && (r_step == 0.0) && (R_step == 0.0) && (theta_step > 0.0)) {
-		for (mut<u32> p = 0; p < theta_count; ++p) {
-			f64 theta = theta_min + as_f64(p_min + p)*theta_step;
+	if ((lambda_list.count() == 0) && (r_list.count() == 0) && (R_list.count() == 0) && (theta_list.count() > 0)) {
+		print::line<PAD>("# theta", "PES");
 
-			f64 v = pes.value(arrang, r_min, R_min, theta);
+		for (f64 theta : theta_list.as_range_inclusive()) {
+			f64 v = pes.value(arrang, r_list.min, R_list.min, theta);
 
-			print::line<PAD>(theta, ' ', (v + shift)*scale);
+			print::line<PAD>(theta, SHIFT_AND_SCALE(v));
 		}
 	}
 
 	//
-	// R-only print:
+	// R-only printing:
 	//
 
-	else if ((lambda_step == 0) && (r_step == 0.0) && (R_step > 0.0) && (theta_step == 0.0)) {
-		for (mut<u32> m = 0; m < R_count; ++m) {
-			f64 R = R_min + as_f64(m_min + m)*R_step;
+	else if ((lambda_list.count() == 0) && (r_list.count() == 0) && (R_list.count() > 0) && (theta_list.count() == 0)) {
+		print::line<PAD>("# R (a.u.)", "PES");
 
-			f64 v = pes.value(arrang, r_min, R, theta_min);
+		for (f64 R : R_list) {
+			f64 v = pes.value(arrang, r_list.min, R, theta_list.min);
 
-			print::line<PAD>(R, ' ', (v + shift)*scale);
+			print::line<PAD>(R, SHIFT_AND_SCALE(v));
 		}
 	}
 
 	//
-	// r-only print (with rot. barrier):
+	// r-only printing (with j-dependent centrifugal barrier):
 	//
 
-	else if ((lambda_step == 0) && (r_step > 0.0) && (R_step == 0.0) && (theta_step == 0.0)) {
-		for (mut<u32> j = j_min; j <= j_max; j += j_step) {
+	else if ((lambda_list.count() == 0) && (r_list.count() > 0) && (R_list.count() == 0) && (theta_list.count() == 0)) {
+		for (u32 j : j_list) {
 			print::line("# j = ", j);
+			print::line<PAD>("# r (a.u.)", "PES");
 
-			for (mut<u32> n = 0; n < r_count; ++n) {
-				f64 r = r_min + as_f64(n_min + n)*r_step;
-
+			for (f64 r : r_list) {
 				mut<f64> v = 0.0;
 
 				switch (arrang) {
@@ -159,7 +103,7 @@ int main()
 					case 'c': v = pes.diatom_ab(j, r); break;
 				}
 
-				print::line<PAD>(r, ' ', (v + shift)*scale);
+				print::line<PAD>(r, SHIFT_AND_SCALE(v));
 			}
 
 			print::line();
@@ -167,19 +111,17 @@ int main()
 	}
 
 	//
-	// (R, theta) print:
+	// (R, theta) printing:
 	//
 
-	else if ((lambda_step == 0) && (r_step == 0.0) && (R_step > 0.0) && (theta_step > 0.0)) {
-		for (mut<u32> m = 0; m < R_count; ++m) {
-			f64 R = R_min + as_f64(m_min + m)*R_step;
+	else if ((lambda_list.count() == 0) && (r_list.count() == 0) && (R_list.count() > 0) && (theta_list.count() > 0)) {
+		print::line<PAD>("# R (a.u.)", "theta", "PES");
 
-			for (mut<u32> p = 0; p < theta_count; ++p) {
-				f64 theta = theta_min + as_f64(p_min + p)*theta_step;
+		for (f64 R : R_list) {
+			for (f64 theta : theta_list.as_range_inclusive()) {
+				f64 v = pes.value(arrang, r_list.min, R, theta);
 
-				f64 v = pes.value(arrang, r_min, R, theta);
-
-				print::line<PAD>(R, ' ', theta, ' ', (v + shift)*scale);
+				print::line<PAD>(R, theta, SHIFT_AND_SCALE(v));
 			}
 
 			print::line();
@@ -187,19 +129,17 @@ int main()
 	}
 
 	//
-	// (r, theta) print:
+	// (r, theta) printing:
 	//
 
-	else if ((lambda_step == 0) && (r_step > 0.0) && (R_step == 0.0) && (theta_step > 0.0)) {
-		for (mut<u32> n = 0; n < r_count; ++n) {
-			f64 r = r_min + as_f64(n_min + n)*r_step;
+	else if ((lambda_list.count() == 0) && (r_list.count() > 0) && (R_list.count() == 0) && (theta_list.count() > 0)) {
+		print::line<PAD>("# r (a.u.)", "theta", "PES");
 
-			for (mut<u32> p = 0; p < theta_count; ++p) {
-				f64 theta = theta_min + as_f64(p_min + p)*theta_step;
+		for (f64 r : r_list) {
+			for (f64 theta : theta_list.as_range_inclusive()) {
+				f64 v = pes.value(arrang, r, R_list.min, theta);
 
-				f64 v = pes.value(arrang, r, R_min, theta);
-
-				print::line<PAD>(r, ' ', theta, ' ', (v + shift)*scale);
+				print::line<PAD>(r, theta, SHIFT_AND_SCALE(v));
 			}
 
 			print::line();
@@ -207,19 +147,17 @@ int main()
 	}
 
 	//
-	// (r, R) print:
+	// (r, R) printing:
 	//
 
-	else if ((lambda_step == 0) && (r_step > 0.0) && (R_step > 0.0) && (theta_step == 0.0)) {
-		for (mut<u32> n = 0; n < r_count; ++n) {
-			f64 r = r_min + as_f64(n_min + n)*r_step;
+	else if ((lambda_list.count() == 0) && (r_list.count() > 0) && (R_list.count() > 0) && (theta_list.count() == 0)) {
+		print::line<PAD>("# r (a.u.)", "R (a.u.)", "PES");
 
-			for (mut<u32> m = 0; m < R_count; ++m) {
-				f64 R = R_min + as_f64(m_min + m)*R_step;
+		for (f64 r : r_list) {
+			for (f64 R : R_list) {
+				f64 v = pes.value(arrang, r, R, theta_list.min);
 
-				f64 v = pes.value(arrang, r, R, theta_min);
-
-				print::line<PAD>(r, ' ', R, ' ', (v + shift)*scale);
+				print::line<PAD>(r, R, SHIFT_AND_SCALE(v));
 			}
 
 			print::line();
@@ -227,22 +165,19 @@ int main()
 	}
 
 	//
-	// Legendre multipoles print:
+	// Legendre multipole printing:
 	//
 
-	else if (lambda_step > 0) {
-		for (mut<u32> lambda = lambda_min; lambda <= lambda_max; lambda += lambda_step) {
-			for (mut<u32> n = 0; n < r_count; ++n) {
-				f64 r = r_min + as_f64(n_min + n)*r_step;
+	else if (lambda_list.count() > 0) {
+		for (u32 lambda : lambda_list.as_range_inclusive()) {
+			for (f64 r : r_list) {
+				print::line("# lambda = ", lambda, ", r = ", r, " a.u.");
+				print::line<PAD>("# R (a.u.)", "PES");
 
-				print::line("# lambda = ", lambda, ", r = ", r);
-
-				for (mut<u32> m = 0; m < R_count; ++m) {
-					f64 R = R_min + as_f64(m_min + m)*R_step;
-
+				for (f64 R : R_list) {
 					f64 v = pes.legendre_multipole_term(arrang, lambda, r, R);
 
-					print::line<PAD>(R, ' ', (v + shift)*scale);
+					print::line<PAD>(R, SHIFT_AND_SCALE(v));
 				}
 
 				print::line();
@@ -251,22 +186,18 @@ int main()
 	}
 
 	//
-	// (r, R, theta) print:
+	// (r, R, theta) printing:
 	//
 
 	else {
-		for (mut<u32> n = 0; n < r_count; ++n) {
-			f64 r = r_min + as_f64(n_min + n)*r_step;
+		print::line<PAD>("# r (a.u.)", "R (a.u.)", "theta", "PES");
 
-			for (mut<u32> m = 0; m < R_count; ++m) {
-				f64 R = R_min + as_f64(m_min + m)*R_step;
-
-				for (mut<u32> p = 0; p <= theta_count; ++p) {
-					f64 theta = theta_min + as_f64(p_min + p)*theta_step;
-
+		for (f64 r : r_list) {
+			for (f64 R : R_list) {
+				for (f64 theta : theta_list.as_range_inclusive()) {
 					f64 v = pes.value(arrang, r, R, theta);
 
-					print::line<PAD>(r, ' ', R, ' ', theta, ' ', (v + shift)*scale);
+					print::line<PAD>(r, R, theta, SHIFT_AND_SCALE(v));
 				}
 
 				print::line();
